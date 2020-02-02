ec9b19cd56aef27b87a65023804166c13dc74e1a
frontendgod <3054750147@qq.com>
2020-02-02 15:38:15 +0800

    p20

--- pages/detailed.js
+++ pages/detailed.js
@@ -12,43 +12,29 @@ import MarkNav from 'markdown-navbar';
 import 'markdown-navbar/dist/navbar.css';
 import axios from 'axios'
 
+import marked from 'marked'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/monokai-sublime.css'
 
-const Detailed = () => {
 
-	let markdown='# P01:课程介绍和环境搭建\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-   '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n'+
-  '\`console.log(111)\` \n\n'+
-  '# p02:来个Hello World 初始Vue3.0\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n'+
-  '***\n\n\n' +
-  '# p03:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p04:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '#5 p05:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p06:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p07:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '``` var a=11; ```'
+const Detailed = (props) => {
+
+	const renderer = new marked.Renderer()
+
+    marked.setOptions({
+        renderer: renderer,
+        gfm: true,
+        pedantic: false,
+        sanitize: false,
+        tables: true,
+        breaks: false,
+        smartLists: true,
+        highlight: function(code) {
+            return hljs.highlightAuto(code).value
+        }
+    })
+
+    let html = marked(props.article_content)
 	
 	return(
 		<div>
@@ -70,18 +56,15 @@ const Detailed = () => {
 					<div className="detailed-title">
 						React实战视频教程-技术胖Blog开发(更新第10集)
 					</div>
-
 					<div className="list-icon center">
 						<span><Icon type="calendar" /> 2019-06-28</span>
 						<span><Icon type="folder" /> 视频教程</span>
 						<span><Icon type="fire" /> 5498人</span>
 					</div>
-
-					<div className="detailed-content" >
-						<ReactMarkdown 
-							source={markdown} 
-							escapeHtml={false}  
-						/>
+					<div className="detailed-content" 
+                        dangerouslySetInnerHTML={{ __html: html }}
+                    >
+                        
 					</div>
 
 						</div>
@@ -97,7 +80,7 @@ const Detailed = () => {
 								<div className="nav-title">文章目录</div>
 								<MarkNav
 									className="article-menu"
-									source={markdown}
+									source={html}
 									ordered={false}
 								/>
 							</div>
